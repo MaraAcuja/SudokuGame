515c3572e5ce326bbbd2d6ef79be6515941a6a48
Mara Acuja <maracujagithub@gmail.com>
2022-01-15 12:53:26 +0100

    # Updated ddd file

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
+use_plugin("python.sphinx")
 
 
 name = "SudokuGame"
@@ -14,4 +15,10 @@ default_task = "publish"
 
 @init
 def set_properties(project):
+    project.depends_on('unittest')
+    project.depends_on('numpy')
+    project.set_property('sphinx_config_path', 'docs/')
+    project.set_property('sphinx_source_dir', 'docs/source')
+    project.set_property('sphinx_output_dir', 'docs/_build')
     project.set_property("coverage_break_build", False)
+
